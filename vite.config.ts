2da60c8d347d49fb07b2acf73c8b60c235c75c91
shg95 <43364549+shg95@users.noreply.github.com>
2025-08-13 22:44:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: '/',
-  build: { outDir: 'docs' },
+  base: '/', // user/org repo (pokerapp.github.io) uses root
   plugins: [react()],
+  resolve: { alias: { '@': '/src' } },
+  server: { port: 5173, strictPort: true },
+  preview: { port: 4173, strictPort: true },
 })
