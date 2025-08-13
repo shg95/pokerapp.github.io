f0dd2799713c36608f2a97502a2e314c9ae2c737
shg95 <43364549+shg95@users.noreply.github.com>
2025-08-13 22:24:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // user/org repo uses root
+  base: '/', // user/org pages use root
   plugins: [react()],
   resolve: { alias: { '@': '/src' } },
   server: { port: 5173, strictPort: true },
